acd1c1354c266b49129bb2785178a47d349ee483
m4r() <50446006+chauneng@users.noreply.github.com>
2025-10-28 21:11:26 +0900

    add: query summary dto
    Add review query summary dto and some chores lie fix some object names to meet name convention.

---
+++ src/dto/steam_data/review_query_summary.py
@@ -0,0 +1,17 @@
+"""Result from querying Steam for review summary data."""
+
+from pydantic import BaseModel, Field
+
+
+class ReviewQuerySummaryDTO(BaseModel):
+    """Result from querying Steam for review summary data."""
+
+    id: int = Field(..., description="Review Query Summary ID")
+    game_id: int = Field(..., description="Steam App ID of the game")
+    num_reviews: int = Field(..., description="Number of reviews in the query result")
+    review_score: int = Field(..., description="Overall review score")
+    review_score_desc: str = Field(..., description="Description of the review score")
+    total_positive: int = Field(..., description="Total number of positive reviews")
+    total_negative: int = Field(..., description="Total number of negative reviews")
+    total_reviews: int = Field(..., description="Total number of reviews for the game")
+    cursor: str = Field(..., description="Cursor for pagination")
