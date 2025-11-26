8ccb8ca6a5adfb0cbe36f04433f654be2bc1624e
m4r() <50446006+chauneng@users.noreply.github.com>
2025-11-26 09:58:44 +0900

    fix: review scrapper to check db
    Fix review scrapper to check game's data from database

--- src/scraper.py
+++ src/scraper.py
@@ -6,6 +6,8 @@ from .dto_handler import (
 )
 from .database_manager import DatabaseManager
 from .http_client_manager import HttpClientManager
+from .repositories import GameRepo
+from .repositories import QuerySummaryRepo
 
 
 async def scrape_reviews(
@@ -20,18 +22,32 @@ async def scrape_reviews(
         gathered_review (int): The number of reviews gathered for the specified app_id.
     """
     http_conn = http_manager.get_client("default")
-    # db_conn = db_manager.get_connection("default")
+    db_session_factory = db_manager.get_session_factory("default")
 
     base_url = f"https://store.steampowered.com/appreviews/{app_id}"
     params = "?json=1&filter=recent&language=all"
-    # cursor = "*"
 
-    review_gathered = 0
+    async with db_session_factory() as session:
+        querysummary_repo = QuerySummaryRepo(session)
+        game_repo = GameRepo(session)
 
-    response = steam_response_dto(await http_conn.get(base_url + params))
-    if response.success == 1:
-        review_gathered = len(response.reviews)
+        previous_summary = await querysummary_repo.get_query_summary(app_id)
+        if previous_summary is not None:
+            cursor = previous_summary.cursor
+            params += f"&cursor={cursor}"
+        else:
+            await game_repo.add_game(app_id)
+
+        print(f"Fetching reviews from URL: {base_url + params}")
+        response = steam_response_dto(await http_conn.get(base_url + params))
+
+        review_gathered = 0
+
+        if response.success == 0:
+            return review_gathered
+
+        review_gathered += len(response.reviews)
         persistance_dto = persistance_create_dto(app_id=app_id, response_dto=response)
-        print(f"{persistance_dto}")
+        await querysummary_repo.add_query_summary(persistance_dto.query_summary)
 
     return review_gathered
